f296b57c8b6ae2c0c8fee60a1593cd840fd2b00c
Andreas Ulvund <ulvund_a@hotmail.com>
2023-11-19 22:34:19 +0100

    adding edit post

--- src/js/ui/posts.js
+++ src/js/ui/posts.js
@@ -25,17 +25,39 @@ async function displayPosts(posts) {
 
   posts.forEach((post) => {
     const postDiv = document.createElement("div");
-    postDiv.className = "col mb-3";
+    postDiv.className = "post-card col mb-3 position-relative";
+    const postImage = post.media
+      ? `<img src="${post.media}" class="card-img-top" alt="Post image">`
+      : "";
+
+    const editButtonHTML = post.author.name.includes("Ulvounth")
+      ? `<button class="edit-post btn btn-secondary position-absolute top-0 end-0 m-3">
+            <i class="fa-solid fa-pencil"></i>
+          </button>`
+      : "";
+
     postDiv.innerHTML = `
-        <div class="gallery-image">
+        ${editButtonHTML}
+        <a href="post.html?postId=${
+          post.id
+        }" class="text-decoration-none text-dark">
+          <div class="gallery-image">
             <h5 class="card-title">${post.title}</h5>
-            <p class="card-text">${post.body}</p>
-            <footer>
-                <small>By ${post.author.name} ${post.created}</small>
-            </footer>
-            <a href="post.html?postId=${post.id}" class="btn btn-primary mt-2">View Post</a>
-        </div>
-    `;
+            ${postImage}
+            <p class="card-text p-4">${post.body}</p>
+            <div class="post-meta">
+              <small class="text-muted">By ${post.author.name} - ${new Date(
+      post.created
+    ).toLocaleTimeString()}</small>
+            </div>
+            <div class="post-actions">
+              <span class="like-count">${post.reactions.length} likes</span>
+              <button class="btn btn-primary btn-sm">Like</button>
+              <button class="btn btn-secondary btn-sm">Comment</button>
+            </div>
+          </div>
+        </a>
+      `;
 
     // Check if the delete button should be added
     if (post.author.name.includes("Ulvounth")) {
@@ -47,10 +69,64 @@ async function displayPosts(posts) {
     }
 
     postsContainer.appendChild(postDiv);
+
+    // If there is an edit button, attach the click event listener to it
+    const editButtons = postDiv.getElementsByClassName("edit-post");
+    if (editButtons.length > 0) {
+      editButtons[0].addEventListener("click", (e) => {
+        e.preventDefault(); // Prevents navigation when clicking the button
+        e.stopPropagation(); // Stops the click from reaching the <a> tag
+        editPost(post); // Call editPost function with the current post
+      });
+    }
   });
+
+  function editModal(post) {
+    console.log(post);
+    const modalHTML = `
+          <div class="modal fade" id="editPost" tabindex="-1" aria-labelledby="editPostLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="editPostLabel">Edit Post</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                          <form id="createPostForm">
+                              <div class="mb-3">
+                                  <label for="postTitle" class="form-label">Title</label>
+                                  <input type="text" class="form-control" id="postTitle" value="${post.title}" required>
+                              </div>
+                              <div class="mb-3">
+                                  <label for="postBody" class="form-label">Body</label>
+                                  <textarea class="form-control" id="postBody" required>${post.body}</textarea>
+                              </div>
+                              <div class="mb-3">
+                                  <label for="postMedia" class="form-label">Media URL</label>
+                                  <input type="url" class="form-control" id="postMedia" value="${post.media}">
+                              </div>
+                              <button type="submit" class="btn btn-primary">Edit Post</button>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      `;
+
+    document.body.insertAdjacentHTML("beforeend", modalHTML);
+
+    setupFormSubmission();
+  }
+
+  function editPost(post) {
+    editModal(post);
+    let editPost = new bootstrap.Modal(document.getElementById("editPost"));
+    editPost.show();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
   const posts = await fetchPosts();
+
   displayPosts(posts);
 });
