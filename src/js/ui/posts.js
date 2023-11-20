e1ed3551d6dfcb96ba1c0e1a8b081040b1d7e441
Andreas Ulvund <ulvund_a@hotmail.com>
2023-11-20 09:43:49 +0100

    Update posts.js

--- src/js/ui/posts.js
+++ src/js/ui/posts.js
@@ -83,48 +83,101 @@ async function displayPosts(posts) {
 
   function editModal(post) {
     console.log(post);
+    // Generate a unique ID for the modal and form
+    const modalId = `editPostModal-${post.id}`;
+    const formId = `editPostForm-${post.id}`;
+
     const modalHTML = `
-          <div class="modal fade" id="editPost" tabindex="-1" aria-labelledby="editPostLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                  <div class="modal-content">
-                      <div class="modal-header">
-                          <h5 class="modal-title" id="editPostLabel">Edit Post</h5>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                          <form id="createPostForm">
-                              <div class="mb-3">
-                                  <label for="postTitle" class="form-label">Title</label>
-                                  <input type="text" class="form-control" id="postTitle" value="${post.title}" required>
-                              </div>
-                              <div class="mb-3">
-                                  <label for="postBody" class="form-label">Body</label>
-                                  <textarea class="form-control" id="postBody" required>${post.body}</textarea>
-                              </div>
-                              <div class="mb-3">
-                                  <label for="postMedia" class="form-label">Media URL</label>
-                                  <input type="url" class="form-control" id="postMedia" value="${post.media}">
-                              </div>
-                              <button type="submit" class="btn btn-primary">Edit Post</button>
-                          </form>
-                      </div>
-                  </div>
-              </div>
+      <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="editPostLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editPostLabel">Edit Post</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form id="${formId}">
+                <div class="mb-3">
+                  <label for="postTitle" class="form-label">Title</label>
+                  <input type="text" class="form-control" id="postTitle" value="${
+                    post.title
+                  }" required>
+                </div>
+                <div class="mb-3">
+                  <label for="postBody" class="form-label">Body</label>
+                  <textarea class="form-control" id="postBody" required>${
+                    post.body
+                  }</textarea>
+                </div>
+                <div class="mb-3">
+                  <label for="postMedia" class="form-label">Media URL</label>
+                  <input type="url" class="form-control" id="postMedia" value="${
+                    post.media || ""
+                  }">
+                </div>
+                <button type="submit" class="btn btn-primary">Edit Post</button>
+              </form>
+            </div>
           </div>
-      `;
+        </div>
+      </div>
+    `;
 
     document.body.insertAdjacentHTML("beforeend", modalHTML);
 
-    setupFormSubmission();
+    // Now set up the form submission
+    setupFormSubmission(post, formId, modalId);
+  }
+
+  function setupFormSubmission(post, formId, modalId) {
+    const form = document.getElementById(formId);
+    form.onsubmit = async function (e) {
+      e.preventDefault();
+
+      // Get the values from the form
+      const title = document.getElementById("postTitle").value;
+      const body = document.getElementById("postBody").value;
+      const media = document.getElementById("postMedia").value;
+
+      const accessToken = localStorage.getItem("accessToken");
+
+      try {
+        const response = await fetch(
+          `https://api.noroff.dev/api/v1/social/posts/${post.id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({ title, body, media }),
+          }
+        );
+
+        if (response.ok) {
+          // Handle successful update here...
+          // For example, refresh the post list or update the UI accordingly
+          const modalElement = document.getElementById(modalId);
+          const modalInstance = bootstrap.Modal.getInstance(modalElement);
+          modalInstance.hide();
+          modalElement.remove(); // Clean up the modal from DOM after closing
+        } else {
+          // Handle errors, such as displaying a message to the user
+        }
+      } catch (error) {
+        console.error("Error updating post:", error);
+      }
+    };
   }
 
   function editPost(post) {
     editModal(post);
-    let editPost = new bootstrap.Modal(document.getElementById("editPost"));
-    editPost.show();
+    let modal = new bootstrap.Modal(
+      document.getElementById(`editPostModal-${post.id}`)
+    );
+    modal.show();
   }
 }
-
 document.addEventListener("DOMContentLoaded", async () => {
   const posts = await fetchPosts();
 
